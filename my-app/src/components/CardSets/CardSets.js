412397a9ef410dc45dfc789b85721b789b63b263
Loy <loyhernandez59@gmail.com>
2018-03-01 15:06:05 -0600

    added button

--- my-app/src/components/CardSets/CardSets.js
+++ my-app/src/components/CardSets/CardSets.js
@@ -6,44 +6,57 @@ class CardSets extends Component{
     constructor(props){
         super(props);
 
-        this.state={
+        this.state={   
+            set: [] 
+        // set:    [ "Basic",
+        //         "Classic",
+        //         "Promo",
+        //         "Hall of Fame",
+        //         "Naxxramas",
+        //         "Goblins vs Gnomes",
+        //         "Blackrock Mountain",
+        //         "The Grand Tournament",
+        //         "The League of Explorers",
+        //         "Whispers of the Old Gods",
+        //         "One Night in Karazhan",
+        //         "Mean Streets of Gadgetzan",
+        //         "Journey to Un'Goro",
+        //         "Knights of the Frozen Throne",
+        //         "Kobolds & Catacombs",
+        //         "Tavern Brawl",
+        //         "Hero Skins",
+        //         "Missions",
+        //         "Credits",
+        //         "System",
+        //         "Debug" ]
 
         }
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    componentWillMount(){
-        axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/set/'+"?"+ config.key)
+    handleChange(event){
+        event.preventDefault();
+        //this.setState({value: event.target.value});
+        axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/set/'+ event.target.value+"?"+ config.key)
         .then(res => {
             console.log(res.data)
         })
     }
-    // "Basic",
-    // "Classic",
-    // "Promo",
-    // "Hall of Fame",
-    // "Naxxramas",
-    // "Goblins vs Gnomes",
-    // "Blackrock Mountain",
-    // "The Grand Tournament",
-    // "The League of Explorers",
-    // "Whispers of the Old Gods",
-    // "One Night in Karazhan",
-    // "Mean Streets of Gadgetzan",
-    // "Journey to Un'Goro",
-    // "Knights of the Frozen Throne",
-    // "Kobolds & Catacombs",
-    // "Tavern Brawl",
-    // "Hero Skins",
-    // "Missions",
-    // "Credits",
-    // "System",
-    // "Debug"
+
+    componentWillMount(){
+        axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/set/'+this.state.set+"?"+ config.key)
+        .then(res => {
+            console.log(res.data)
+        })
+    }
+
     render(){
         return(
             <div>
                 <div>
-                    <button>Kobolds & Catacombs</button>
+                    <button value="Kobolds & Catacombs" onClick={this.handleChange} >Kobolds & Catacombs</button>
                 </div>
+                {console.log(this.state)}
             </div>
         )
     }
